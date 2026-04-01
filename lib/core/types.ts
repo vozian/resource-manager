6d6d7fe91f3a5fa4a27d79d2e0273c2fbcb5f67c
valentin <vozian.valentin@gmail.com>
2026-04-01 16:41:22 +0200

    🎨 Update types

--- lib/core/types.ts
+++ lib/core/types.ts
@@ -4,8 +4,23 @@ export type EntityI = {
   updatedAt: number;
 };
 
+export type NamedEntityI = EntityI & {
+  name: string;
+};
+
 export type ParameterTypeI = NamedEntityI & {
   valueType: ParameterValueTypeI;
+};
+
+export type ResourceTypeI = NamedEntityI & {
+  constraintParameters: ParameterSetI;
+  quantityParameterTypeIds: string[];
+};
+
+export type JobI = NamedEntityI & {
+  notes: string;
+  mappings: ResourceMappingI[];
+  common: ResourceQuantityI[];
 };
 
 export type ParameterI = {
@@ -13,17 +28,8 @@ export type ParameterI = {
   value: unknown;
 };
 
-export type NamedEntityI = EntityI & {
-  name: string;
-};
-
 export type ParameterTypeSetI = ParameterTypeI[];
 export type ParameterSetI = ParameterI[];
-
-export type ResourceTypeI = NamedEntityI & {
-  constraintParameters: ParameterSetI;
-  quantityParameterTypeIds: string[];
-};
 
 export type ResourceQuantityI = {
   resourceTypeId: string;
@@ -43,10 +49,4 @@ export enum ParameterValueTypeI {
 export type ResourceMappingI = {
   inputs: StatusResourceQuantityI[];
   outputs: ResourceQuantityI[];
-};
-
-export type JobI = NamedEntityI & {
-  notes: string;
-  mappings: ResourceMappingI[];
-  common: ResourceQuantityI[];
 };
